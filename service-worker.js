a537e04c450cff90c5e3b44a0d53e052e1a1a463
Dizzle Holy Arts <allenbeene129@yahoo.com>
2024-07-05 12:49:35 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Beenes129.github.io/myOfficialWebsite/precache-manifest.62cd7f53134e199d0e5c346ed1f9031e.js"
+  "/precache-manifest.e826893607a960f8f42cdd065254025d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Beenes129.github.io/myOfficialWebsite/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
