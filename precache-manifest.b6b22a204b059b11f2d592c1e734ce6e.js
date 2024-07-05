17164fb12c68800439ce46b31736d6a6ac0683cb
Dizzle Holy Arts <allenbeene129@yahoo.com>
2024-07-05 12:54:39 -0700

    Updates

---
+++ precache-manifest.b6b22a204b059b11f2d592c1e734ce6e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a527f1a2ee558eb630e7ad083f5acf44",
+    "url": "/myOfficialWebsite/index.html"
+  },
+  {
+    "revision": "da97471abcffe1e4bf88",
+    "url": "/myOfficialWebsite/static/css/main.41a97002.chunk.css"
+  },
+  {
+    "revision": "4aadae2f8179c5cb0651",
+    "url": "/myOfficialWebsite/static/js/2.84c91f87.chunk.js"
+  },
+  {
+    "revision": "ae6bb62977b25ec73f0509e3cbccba19",
+    "url": "/myOfficialWebsite/static/js/2.84c91f87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da97471abcffe1e4bf88",
+    "url": "/myOfficialWebsite/static/js/main.5499eb88.chunk.js"
+  },
+  {
+    "revision": "36a6d8b045345911445d",
+    "url": "/myOfficialWebsite/static/js/runtime-main.7cca19ed.js"
+  },
+  {
+    "revision": "70d4ad7a4c926acf36be798fcb3bcd35",
+    "url": "/myOfficialWebsite/static/media/AllenBeenelogo.70d4ad7a.png"
+  },
+  {
+    "revision": "ffc718cd15e8caac3542af07605bf386",
+    "url": "/myOfficialWebsite/static/media/PAPYRUS.ffc718cd.TTF"
+  },
+  {
+    "revision": "a19f2b67a0540f784bf091db8e36c82a",
+    "url": "/myOfficialWebsite/static/media/calculator.a19f2b67.jpg"
+  },
+  {
+    "revision": "ccf8780828468d7e812c2c2bad170da0",
+    "url": "/myOfficialWebsite/static/media/cover-image.ccf87808.png"
+  },
+  {
+    "revision": "653c2b3d29f66a3c6e957518f3bd16f0",
+    "url": "/myOfficialWebsite/static/media/led-wall.653c2b3d.jpg"
+  },
+  {
+    "revision": "ed494730868bd902e0ee98f1df1877e2",
+    "url": "/myOfficialWebsite/static/media/pastel-puzzels.ed494730.jpg"
+  },
+  {
+    "revision": "938f8fe0fe2e5880b3ef528c5188a096",
+    "url": "/myOfficialWebsite/static/media/run-buddy.938f8fe0.jpg"
+  },
+  {
+    "revision": "12557c9b108cf5e5347f8d838014b31e",
+    "url": "/myOfficialWebsite/static/media/surf-report.12557c9b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e826893607a960f8f42cdd065254025d.js"
+  "/myOfficialWebsite/precache-manifest.b6b22a204b059b11f2d592c1e734ce6e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myOfficialWebsite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
